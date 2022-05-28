439b2a57578bc197f74466624fa076f5e4e0fc8b
Ashutosh Sharma <75155585+asharma8602@users.noreply.github.com>
2022-05-29 02:37:26 +0530

    Recommendation Resolve

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ import { db } from '../firebase'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import useFavourites from '../hooks/useFavourites'
-import { orderBy } from 'lodash'
+import _ from 'lodash'
 
 interface Props {
   netflixOriginals: Movie[]
@@ -49,24 +49,51 @@ const Home = ({
   const showModal = useRecoilValue(modalState)
   const list = useList(user?.uid)
   const favourites = useFavourites(user?.uid)
-  const [genres, setGenres] = useState([])
+  const [genres, setGenres] = useState([{ stringValue: '' }])
   const [recommends, setRecommends] = useState<DocumentData[] | Movie[]>([])
   const [similarMovies, setSimilarMovies] = useState<DocumentData[] | Movie[]>(
     []
   )
   if (loading) return null
-
+  
   useEffect(() => {
     if (user) {
       return onSnapshot(
         collection(db, 'customers', user.uid, 'myGenres'),
         (snapshot) => {
-          const tempGenre = snapshot.docs[0]._document.data.value.mapValue.fields.Genres.arrayValue.values
-          setGenres([tempGenre[1].stringValue, tempGenre[2].stringValue, tempGenre[3].stringValue])
+          const tempGenre: any[] =
+            // @ts-ignore
+            snapshot.docs[0]._document.data.value.mapValue.fields.Genres
+              .arrayValue.values
+          console.log(tempGenre)
+          setGenres([
+            tempGenre[1].stringValue,
+            tempGenre[2].stringValue,
+            tempGenre[3].stringValue,
+          ])
         }
       )
     }
   }, [db])
+  function shuffle(array: DocumentData[] | Movie[]): DocumentData[] | Movie[] {
+    let currentIndex = array.length,
+      randomIndex
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex)
+      currentIndex--
+
+      // And swap it with the current element.
+      ;[array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ]
+    }
+
+    return array
+  }
 
   useEffect(() => {
     if (favourites.length <= 3) {
@@ -76,31 +103,42 @@ const Home = ({
             .get(
               `https://api.themoviedb.org/3/discover/movie?api_key=a80f4351a37f1534cfc62363fbbe8382&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1&with_genres=${genre}&with_watch_monetization_types=flatrate`
             )
-            .then((res) => (res.data.results.map((movie: Movie) => recommends.push(movie))))
+            .then((res) =>
+              res.data.results.map((movie: Movie) => {
+                if (recommends.length < 60) {
+                  recommends.push(movie)
+                }
+              })
+            )
         })
         console.log(recommends)
       }
     } else {
-        {
-          favourites.map((recommend) =>
-            axios
-              .get(
-                `https://api.themoviedb.org/3/movie/${recommend.id}/recommendations?api_key=a80f4351a37f1534cfc62363fbbe8382&language=en-US&page=1`
-              )
-              .then((res) => (res.data.results.map((movie: Movie) => recommends.push(movie))))
-          )
-        }
-        {
-          favourites.map((recommend) =>
-            axios
-              .get(
-                `https://api.themoviedb.org/3/movie/${recommend.id}/similar?api_key=a80f4351a37f1534cfc62363fbbe8382&language=en-US&page=1`
-              )
-              .then((res) => (res.data.results.map((movie: Movie) => similarMovies.push(movie))))
-          )
-        }
+      setRecommends([])
+      {
+        favourites.map((recommend) =>
+          axios
+            .get(
+              `https://api.themoviedb.org/3/movie/${recommend.id}/recommendations?api_key=a80f4351a37f1534cfc62363fbbe8382&language=en-US&page=1`
+            )
+            .then((res) =>
+              res.data.results.map((movie: Movie) => recommends.push(movie))
+            )
+        )
       }
-}, [favourites, recommends, similarMovies])
+      {
+        favourites.map((recommend) =>
+          axios
+            .get(
+              `https://api.themoviedb.org/3/movie/${recommend.id}/similar?api_key=a80f4351a37f1534cfc62363fbbe8382&language=en-US&page=1`
+            )
+            .then((res) =>
+              res.data.results.map((movie: Movie) => similarMovies.push(movie))
+            )
+        )
+      }
+    }
+  }, [favourites])
 
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
@@ -112,13 +150,18 @@ const Home = ({
       <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
         <Banner movies={netflixOriginals} />
         <section className="space-y-24">
-          <Row title="Recommendations for You" movies={recommends} />
-          {similarMovies.length>0?
+          <Row
+            title="Recommendations for You"
+            movies={_.uniq(shuffle(recommends).slice(0, 20))}
+          />
+          {similarMovies.length > 0 ? (
             <Row
-            title="Movies Similar to Your Favourites"
-            movies={similarMovies}/>:
-          <></>
-          }
+              title="Movies Similar to Your Favourites"
+              movies={_.uniq(shuffle(similarMovies).slice(0, 20))}
+            />
+          ) : (
+            <></>
+          )}
           <Row title="Trending Now" movies={trendingNow} />
           <Row title="Top Rated" movies={topRated} />
           <Row title="Action Thrillers" movies={actionMovies} />
